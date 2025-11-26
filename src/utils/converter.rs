14d1159c6c3e45c3b4ef3ac6ac2f9306b6ba04fd
JFKongphop <kongphopleo@gmail.com>
2025-11-27 00:34:48 +0700

    add: converter unit pace and second

--- src/utils/converter.rs
+++ src/utils/converter.rs
@@ -58,3 +58,36 @@ pub fn load_and_resize_image(
 
   Ok((resized, width, height))
 }
+
+pub fn string_space(size: usize, index: usize, pace: &str) -> String {
+  let max_digits = count_digits_iterative(size);
+  let current_digits = count_digits_iterative(index);
+  let padding = " ".repeat(max_digits - current_digits + 3); // +1 space separator
+
+  format!("{}{}{}", index, padding, pace)
+}
+
+pub fn convert_pace_to_sec(pace: &str) -> f32 {
+  let (min, sec) = pace.split_once(':').unwrap();
+
+  let minutes: f32 = min.parse().unwrap();
+  let seconds: f32 = sec.parse().unwrap();
+
+  minutes * 60.0 + seconds
+}
+
+fn count_digits_iterative(mut num: usize) -> usize {
+  if num == 0 {
+    return 1; // 0 has one digit
+  }
+  let mut count = 0;
+  while num > 0 {
+    num /= 10; // For decimal digits
+    count += 1;
+  }
+  count
+}
+
+pub fn pace_percentage(numer: f32, denum: f32) -> f32 {
+  (numer / denum)
+}
