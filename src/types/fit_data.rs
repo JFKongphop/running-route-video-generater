b9d1ef3f7fc3e5d045de0906a42a94dd68cfe597
JFKongphop <kongphopleo@gmail.com>
2025-11-27 00:43:42 +0700

    create type for fit data

--- src/types/fit_data.rs
+++ src/types/fit_data.rs
@@ -2,12 +2,12 @@
 pub struct RouteData {
   pub paces: Vec<String>,
   pub gps_points: Vec<(f64, f64)>,
-  pub distances: Vec<f64>
+  pub distances: Vec<f64>,
 }
 
 #[derive(Debug)]
 pub struct LapData {
-  pub avg_heart_rate: Vec<u8>, 
-  pub enhanced_avg_speed: Vec<String>, 
-  pub avg_step_length: Vec<f64>
+  pub avg_heart_rate: Vec<u8>,
+  pub enhanced_avg_speed: Vec<String>,
+  pub avg_step_length: Vec<f64>,
 }
