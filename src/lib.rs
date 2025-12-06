dc1daa464715f84d0551816e5e1b33b72925c232
JFKongphop <kongphopleo@gmail.com>
2025-12-06 23:49:07 +0700

    add: export method to use this lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
 pub mod generators;
 pub mod types;
 pub mod utils;
+
+// Re-export commonly used items at crate root
+pub use generators::route_video::generate_progressive_route_with_config;
+pub use types::route_config;
